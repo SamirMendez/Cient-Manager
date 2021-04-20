da80ef6cacb218cb51980a84d69831052430996c
Samir Méndez <samimmendez@hotmail.es>
2021-04-20 00:10:20 -0400

    En este commit se hizo el formulario de registro para los administradores del sistema. Tambien se creo un archivo para los modelos de datos de autenticación.

--- src/app/core/authentication/register/register.component.ts
+++ src/app/core/authentication/register/register.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { AuthenticationService } from '../../services/authentication/authentication.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,98 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  
+  // Formulario de registro
+  registerForm: FormGroup;
+  // Formulario de registro
+  // Variable para referenciar los modals
+  modalReference: BsModalRef;
+  // Variable para referenciar los modals
+  // Objeto para mostrar la informacion del modal
+  modalData = {
+    modalTitle: null,
+    modalIcon: null,
+    modalDescription: null,
+  };
+  // Objeto para mostrar la informacion del modal
+  // Variable para validar el registro
+  registerSuccess = false;
+  // Variable para validar el registro
+  constructor(private router: Router, private formBuilder: FormBuilder, private modalService: BsModalService,
+              private authService: AuthenticationService) { }
 
   ngOnInit(): void {
+    // Inicializando formulario
+    this.registerForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(35)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern('^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$')]],
+    });
+    // Inicializando formulario
   }
-
+  // Funcion para navegar al login
+  goToLogin(): void {
+    this.router.navigate(['/login']);
+  }
+  // Funcion para navegar al login
+  // Funcion para registrar usuarios administrativos
+  createUser(registerModal: TemplateRef<any>): void {
+    // Enviando datos al servicio
+    const registerData = {
+      name: this.registerForm.value.name,
+      email: this.registerForm.value.email,
+      password: this.registerForm.value.password,
+    };
+    this.authService.createUser(registerData).then((returnedData: any) => {
+      if (returnedData.status === 'success') {
+        this.registerSuccess = true;
+        // Alerntado a los usuarios del registro exitoso
+        this.modalData.modalIcon = '../../../../assets/ilustrations/undraw_done_a34v.svg';
+        this.modalData.modalTitle = '¡Registro exitoso!';
+        this.modalData.modalDescription = 'Gracias por registrarte, accede a tu cuenta y disfruta de nuestras herramientas';
+        this.modalReference = this.modalService.show(registerModal);
+        // Alerntado a los usuarios del registro exitoso
+      } else {
+      const errorCodes = returnedData.data.code;
+      switch (errorCodes) {
+        case 'auth/invalid-email':
+          this.modalData.modalIcon = '../../../../assets/ilustrations/undraw_cancel_u1it.svg',
+          this.modalData.modalTitle = 'Error de autenticación',
+          this.modalData.modalDescription = 'Dirección de correo invalida',
+          this.modalReference = this.modalService.show(registerModal);
+          break;
+        case 'auth/email-already-in-use':
+          this.modalData.modalIcon = '../../../../assets/ilustrations/undraw_cancel_u1it.svg',
+          this.modalData.modalTitle = 'Error de autenticación',
+          this.modalData.modalDescription = 'La direccion de correo especificada esta vinculada a otra cuenta',
+          this.modalReference = this.modalService.show(registerModal);
+          break;
+        case 'auth/operation-not-allowed':
+          this.modalData.modalIcon = '../../../../assets/ilustrations/undraw_cancel_u1it.svg',
+          this.modalData.modalTitle = 'Error de autenticación',
+          this.modalData.modalDescription = 'La operacion de registro ha sido deshabilitada',
+          this.modalReference = this.modalService.show(registerModal);
+          break;
+        case 'auth/weak-password':
+          this.modalData.modalIcon = '../../../../assets/ilustrations/undraw_cancel_u1it.svg',
+          this.modalData.modalTitle = 'Error de autenticación',
+          this.modalData.modalDescription = 'Contraseña débil',
+          this.modalReference = this.modalService.show(registerModal);
+          break;
+        }
+      }
+    });
+    // Enviando datos al servicio
+  }
+  // Funcion para registrar usuarios administrativos
+  // Funcion para ir al login
+  continuousFunction(): void {
+    if (this.registerSuccess === true) {
+      this.modalReference.hide();
+      this.router.navigate(['/login']);
+    } else {
+      this.modalReference.hide();
+    }
+  }
+  // Funcion para ir al login
 }
--- src/app/core/services/authentication/authentication.service.ts
+++ src/app/core/services/authentication/authentication.service.ts
@@ -1,9 +1,70 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs';
+import { RegisterData } from '../../models/authModels';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  constructor(private userAuth: AngularFireAuth, private realtimeDatabase: AngularFireDatabase) { }
+
+  // Metodo publico para crear un nuevo usuario
+  public async createUser(userData: RegisterData): Promise<any> {
+    return await this.userAuth.createUserWithEmailAndPassword(userData.email, userData.password).then((returnedData) => {
+      // Actualizando el objeto del usuario
+      this.userAuth.onAuthStateChanged((authData) => {
+        authData.updateProfile({
+          displayName: userData.name
+        });
+      });
+      // Actualizando el objeto del usuario
+      // Creando registro en la basse de datos
+      const userId = returnedData.user.uid;
+      this.realtimeDatabase.database.ref(`clientManagerPlatform/users/adminUsers/${userId}/`).set({
+        name: userData.name,
+        email: userData.email,
+        uid: returnedData.user.uid,
+        timeStamp: Date.now(),
+        type: 'administrator'
+      });
+      return {status: 'success', data: returnedData};
+      // Creando registro en la basse de datos
+    }).catch((error) => {
+      return {status: 'error', data: error};
+    });
+  }
+  // Metodo publico para crear un nuevo usuario
+  // Metodo publico para crear iniciar la sesion de un usuario
+  public async loginUser(userData: any): Promise<any> {
+    return await this.userAuth.signInWithEmailAndPassword(userData.email, userData.password).then((returnedData) => {
+      return {status: 'success', data: returnedData};
+    }).catch((error) => {
+      return {status: 'error', data: error};
+    });
+  }
+  // Metodo publico para crear iniciar la sesion de un usuario
+  // Extrayendo datos del usuario
+  public returnUserData(userData: any): Observable<ClientData> {
+    return this.realtimeDatabase.object<ClientData>(`clientManagerPlatform/users/adminUsers/${userData}/`).valueChanges();
+  }
+  // Extrayendo datos del usuario
+  // Funcion para recuperar las cuentas del usuario
+  public async recoverUser(email): Promise<any> {
+    return await this.userAuth.sendPasswordResetEmail(email).then((returnedData: any) => {
+      return returnedData;
+    }).catch((error) => {
+      return error;
+    });
+  }
+  // Funcion para recuperar las cuentas del usuario
+  // Funcion para cerrar la sesion del usuario
+  public async logOut(): Promise<any> {
+    return await this.userAuth.signOut().then((returnedData) => {
+      return returnedData;
+    });
+  }
+  // Funcion para cerrar la sesion del usuario
 }
